1a0fd9fe209f7b4708a616d90a6464be0ca8e3f4
tiga1207@naver.com <tiga1207@naver.com>
2023-08-09 18:10:18 +0900

    추가로 푼 3문제

---
+++ 기초 100제/c++/1029.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+#include<iomanip>
+int main(){
+    double a;
+    std::cin>>a;
+    std::cout<<std::fixed<<std::setprecision(11)<<a;
+}
--- 기초 100제/c++/1030.cpp
+++ 기초 100제/c++/1030.cpp
@@ -1,6 +1,6 @@
 #include<iostream>
 int main(){
-    unsigned int a;
+    long long int a;
     std::cin>>a;
     std::cout<<a;
 }
