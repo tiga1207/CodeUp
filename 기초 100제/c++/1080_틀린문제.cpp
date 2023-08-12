a3a10c67303acde6c6813c3cb28fa10b517562d8
tiga1207@naver.com <tiga1207@naver.com>
2023-08-12 15:15:31 +0900

    80번 문제 해결완

---
+++ 기초 100제/c++/1080_틀린문제.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+    int a,i,sum=0;
+    cin>>a;
+    while(true){    
+        sum+=i;
+        if(sum>=a){
+            cout<<i;
+            break;
+        }else{
+            i++;
+        }
+    }
+}
